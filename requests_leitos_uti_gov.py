e88a0ce633653d738c67c71f66ab59cbcf930569
Darley Soares <53305419+DarleySoares@users.noreply.github.com>
2020-04-17 10:14:04 -0300

    Update requests_leitos_uti_gov.py

--- requests_leitos_uti_gov.py
+++ requests_leitos_uti_gov.py
@@ -2,23 +2,24 @@ import requests
 from datetime import date
 import pandas as pd
 
-# Função para retorna a quantidade de leitos UTI's por estado
-def Leitos_UTI(uf):
-    http = 'https://covid-insumos.saude.gov.br/paineis/insumos/graficos/uti.php?uf=' + uf + ''
+# Função que realiza a captura de todos os dados, deve-se passar o tipo de dado e o número do estado 
+def request_data(tipo, uf):
+    if tipo == 'totalLeitos':      
+        http = 'https://covid-insumos.saude.gov.br/paineis/insumos/graficos/uti.php?uf=' + uf + ''
+    elif tipo == 'leitosAlocados':
+        http = 'https://covid-insumos.saude.gov.br/paineis/insumos/graficos/graflocados.php?uf=' + uf + ''
+    elif tipo == 'respiradores':
+        http = 'https://covid-insumos.saude.gov.br/paineis/insumos/graficos/respiradores.php?uf=' + uf + ''
+    elif tipo == 'leitosSUS':
+        http = 'https://covid-insumos.saude.gov.br/paineis/insumos/graficos/uti_sus.php?uf=' + uf + ''
+    elif tipo == 'leitosPrivados':
+        http = 'https://covid-insumos.saude.gov.br/paineis/insumos/graficos/uti_n_sus.php?uf=' + uf + ''
+    
     page = requests.get(http)
-    UTI = page.text
-    UTI = UTI[(UTI.find('data: [') + 7):]
-    UTI = UTI[:UTI.find(',')]
-    return UTI
-
-# Função para retornar quantidade de leitos alocados por estado
-def Leitos_Locados(uf):
-    http = 'https://covid-insumos.saude.gov.br/paineis/insumos/graficos/graflocados.php?uf=' + uf + ''
-    page = requests.get(http)
-    UTI_LOC = page.text
-    UTI_LOC= UTI_LOC[(UTI_LOC.find('data: [') + 7):]
-    UTI_LOC = UTI_LOC[:UTI_LOC.find(',')]
-    return UTI_LOC
+    data = page.text
+    data = data[(data.find('data: [') + 7):]
+    data = data[:data.find(',')]
+    return data
 
 # Lista com número dos estados
 estados_num = ['11','12','13','14','15','16','17','21','22','23','24','25','26','27','28','29','21','31','32','33','35','50','51','52','53','41','42','43']
@@ -28,7 +29,7 @@ dic_UF = {
     '11':'RO', '12':'AC', '13':'AM', '14':'RR', '15':'PA', '16':'AP', '17':'TO',
     '21':'MA', '22':'PI', '23':'CE', '24':'RN', '25':'PB', '26':'PE', '27':'AL', '28':'SE', '29':'BA',
     '31':'MG', '32':'ES', '33':'RJ', '35':'SP',
-    '41':'PN', '42':'SC', '43':'RN',
+    '41':'PR', '42':'SC', '43':'RN',
     '50':'MS', '51':'MT', '52':'GO', '53':'DF'
 }
 
@@ -36,12 +37,15 @@ ds = []
 
 for x in estados_num:
     UF = dic_UF[x]
-    LL = Leitos_Locados(x)
-    LU = Leitos_UTI(x)
-    append = [UF, LL, LU]
+    Total_Leitos = request_data('totalLeitos', x)
+    Leitos_Alocados = request_data('leitosAlocados', x)
+    Respiradores = request_data('respiradores', x)
+    Leitos_SUS = request_data('leitosSUS', x)
+    Leitos_Privados = request_data('leitosPrivados', x)
+
+    append = [UF, Leitos_Alocados, Respiradores, Total_Leitos, Leitos_SUS, Leitos_Privados]
     ds.append(append)
 
 # Cria o dataframe
-UTI_SUS = pd.DataFrame(data = ds, columns = ['UF', 'Leitos locados', 'Leitos UTI'])
+UTI_SUS = pd.DataFrame(data = ds, columns = ['UF', 'Leitos locados', 'Respiradores', 'Total de Leitos','Leitos SUS', 'Leitos Privados'])
 print(UTI_SUS)
-
